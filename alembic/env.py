579e163269d8ec34d0a86ee1207f70821aa02598
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-09-21 23:13:11 +0300

    done

--- alembic/env.py
+++ alembic/env.py
@@ -2,19 +2,14 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from asset_management.models import Base  # Importing Base from your models
+from asset_management.models import Base  
 
-# This is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Add your model's MetaData object here for 'autogenerate' support
-target_metadata = Base.metadata  # Set the target metadata
+target_metadata = Base.metadata  
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -53,7 +48,7 @@ def run_migrations_online() -> None:
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata  # Make sure this is correctly placed
+            target_metadata=target_metadata  
         )
 
         with context.begin_transaction():
